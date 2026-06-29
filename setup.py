ba23dc44def8d735f111e09a8e8c8b285489c3ae
Oliver H Page <ronbelina@gmail.com>
2026-06-29 16:19:28 +0100

    console echo for testing

--- setup.py
+++ setup.py
@@ -1,1 +1,4 @@
-print("hello world")
+import sys
+print(f"[setup.py] running under {sys.executable}")
+print(f"[setup.py] version {sys.version_info.major}.{sys.version_info.minor}")
+print(f"[setup.py] args: {sys.argv[1:]}")
