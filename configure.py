28bc15193ece0551e9eb3e32df5ae0b24fcec102
Oliver H Page <ronbelina@gmail.com>
2026-06-29 16:29:24 +0100

    Rename setup.py to configure.py

---
+++ configure.py
@@ -0,0 +1,4 @@
+import sys
+print(f"[configure.py] running under {sys.executable}")
+print(f"[configure.py] version {sys.version_info.major}.{sys.version_info.minor}")
+print(f"[configure.py] args: {sys.argv[1:]}")
--- setup.py
+++
@@ -1,4 +0,0 @@
-import sys
-print(f"[setup.py] running under {sys.executable}")
-print(f"[setup.py] version {sys.version_info.major}.{sys.version_info.minor}")
-print(f"[setup.py] args: {sys.argv[1:]}")
